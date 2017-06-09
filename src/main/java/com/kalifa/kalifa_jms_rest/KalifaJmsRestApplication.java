09b6df2e340fb288c0e45139db4d6563b5a3225f
Renan Verli <git@rverli.com.br>
2017-06-09 18:00:09 -0300

    Initial commit

---
+++ src/main/java/com/kalifa/kalifa_jms_rest/KalifaJmsRestApplication.java
@@ -0,0 +1,12 @@
+package com.kalifa.kalifa_jms_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalifaJmsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalifaJmsRestApplication.class, args);
+	}
+}
